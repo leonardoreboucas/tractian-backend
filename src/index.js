639f970d8bc2595486ea8c3a57e2e8ca9795a5cf
leonardoreboucas <leonardo_r_carvalho@yahoo.com.br>
2022-02-24 14:47:13 -0300

    API v1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
+const net = require('net');
+const client = net.connect({port: 80, host:"google.com"}, () => {
+  console.log('MyIP='+client.localAddress);
+  console.log('MyPORT='+client.localPort);
+});
+
+
+
 const express = require("express");
 const consign = require("consign");
 const db = require("./database/connection").connectToServer(()=>{})
